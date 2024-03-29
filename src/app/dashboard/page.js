ada8f558ef2c4a1856434e92408ce9bdfff9fcaa
Masiar Novine <novinefarahb@uni-potsdam.de>
2024-03-30 00:08:42 +0100

    Include example table

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -5,9 +5,102 @@ import {
   Column,
   FileUploader,
   FileUploaderItem,
+  Pagination
 } from '@carbon/react';
 import { useState } from 'react';
 import React from 'react';
+import SampleDataTable from '@/app/datatable/SampleTable.js'
+
+// Sample table
+const rows = [
+  {
+    id: "1",
+    class: "1st",
+    sex: "Male",
+    age: "Child",
+    survived: "No",
+    freq: "0"
+  },
+  {
+    id: "2",
+    class: "1st",
+    sex: "Male",
+    age: "Child",
+    survived: "No",
+    freq: "0"
+  },
+  {
+    id: "3",
+    class: "1st",
+    sex: "Male",
+    age: "Child",
+    survived: "No",
+    freq: "35"
+  },
+  {
+    id: "4",
+    class: "Crew",
+    sex: "Male",
+    age: "Child",
+    survived: "No",
+    freq: "0"
+  },
+  {
+    id: "5",
+    class: "1st",
+    sex: "Female",
+    age: "Child",
+    survived: "No",
+    freq: "0"
+  },
+  {
+    id: "6",
+    class: "2nd",
+    sex: "Female",
+    age: "Child",
+    survived: "No",
+    freq: "0"
+  },
+  {
+    id: "7",
+    class: "3nd",
+    sex: "Female",
+    age: "Child",
+    survived: "No",
+    freq: "17"
+  },
+  {
+    id: "8",
+    class: "Crew",
+    sex: "Female",
+    age: "Child",
+    survived: "No",
+    freq: "0"
+  }
+]
+
+const headers = [
+  {
+    key: "class",
+    header: "Class"
+  },
+  {
+    key: "sex",
+    header: "Sex"
+  },
+  {
+    key: "age",
+    header: "Age"
+  },
+  {
+    key: "survived",
+    header: "Survived"
+  },
+  {
+    key: "freq",
+    header: "Freq"
+  }
+]
 
 export function uploadFiles(input, files = []) {
   Object.defineProperty(input, 'files', {
@@ -25,9 +118,10 @@ export function uploadFiles(input, files = []) {
 }
 
 export default function DashboardPage() {
+  const [firstRowIndex, setFirstRowIndex] = useState(0);
+  const [currentPageSize, setCurrentPageSize] = useState(10);
   const [currentStatus, setCurrentStatus] = useState("");
   const [isLoaded, setLoaded] = useState(false);
-
 
   function onFileChanged(file) {
     if (file == "complete") {
@@ -39,8 +133,8 @@ export default function DashboardPage() {
     labelTitle: 'Upload files',
     buttonLabel: 'Add file',
     size: 'md',
-    labelDescription: 'Max file size 1mb. Only .rds files are supported.',
-    accept: ['.rds'],
+    labelDescription: 'Max file size 1mb. Only .json files are supported.',
+    accept: ['.json'],
     multiple: false,
     onChange: onFileChanged(currentStatus),
     filenameStatus: currentStatus,
@@ -66,6 +160,25 @@ export default function DashboardPage() {
       </Column>
       <Column lg={16} md={8} sm={4} className="dashboard-page__r3">
       {/*Bottom*/}
+      <SampleDataTable
+        headers={headers}
+        rows={rows.slice(firstRowIndex, firstRowIndex + currentPageSize)}
+        className="dashboard-page__sample_table"
+      />
+      <Pagination
+        totalItems={rows.length}
+        backwardText='Previous page'
+        forwardText='Next page'
+        pageSize={currentPageSize}
+        pageSizes={[5, 10, 15, 25]}
+        itemsPerPageText='Items per page'
+        onChange={({ page, pageSize  }) => {
+            if (pageSize !== currentPageSize) {
+              setCurrentPageSize(pageSize);
+            }
+            setFirstRowIndex(pageSize * (page - 1));
+        }}
+      />
       </Column>
     </Grid>
   )
